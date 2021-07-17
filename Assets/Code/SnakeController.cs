b76049f32c6d9fd5fe660e1b45ae888ab321494c
One Minute Games <oneminutegamesbusiness@gmail.com>
2021-07-17 14:13:35 +0200

    Initial commit.

---
+++ Assets/Code/SnakeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour {
+
+    // Settings
+    public float MoveSpeed = 5;
+    public float SteerSpeed = 180;
+    public float BodySpeed = 5;
+    public int Gap = 10;
+
+    // References
+    public GameObject BodyPrefab;
+
+    // Lists
+    private List<GameObject> BodyParts = new List<GameObject>();
+    private List<Vector3> PositionsHistory = new List<Vector3>();
+
+    // Start is called before the first frame update
+    void Start() {
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+        GrowSnake();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        // Move forward
+        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+        // Steer
+        float steerDirection = Input.GetAxis("Horizontal"); // Returns value -1, 0, or 1
+        transform.Rotate(Vector3.up * steerDirection * SteerSpeed * Time.deltaTime);
+
+        // Store position history
+        PositionsHistory.Insert(0, transform.position);
+
+        // Move body parts
+        int index = 0;
+        foreach (var body in BodyParts) {
+            Vector3 point = PositionsHistory[Mathf.Clamp(index * Gap, 0, PositionsHistory.Count - 1)];
+
+            // Move body towards the point along the snakes path
+            Vector3 moveDirection = point - body.transform.position;
+            body.transform.position += moveDirection * BodySpeed * Time.deltaTime;
+
+            // Rotate body towards the point along the snakes path
+            body.transform.LookAt(point);
+
+            index++;
+        }
+    }
+
+    private void GrowSnake() {
+        // Instantiate body instance and
+        // add it to the list
+        GameObject body = Instantiate(BodyPrefab);
+        BodyParts.Add(body);
+    }
+}
